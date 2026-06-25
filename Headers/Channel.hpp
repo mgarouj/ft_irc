7f9c966394abbefca51998aa20c9f06dcc0189e3
Ismail Elouarrak <ielouarr@e2r9p9.1337.ma>
2026-06-25 01:37:33 +0100

    add Headers/Channel.hpp

---
+++ Headers/Channel.hpp
@@ -0,0 +1,35 @@
+#ifndef CHANNEL_HPP
+#define CHANNEL_HPP
+
+#include <string>
+#include <vector>
+#include "Client.hpp"
+
+class Channel {
+    private:
+        std::string name;
+        std::string topic;
+        std::string password; // For +k mode
+        std::vector<Client*> members;
+        std::vector<Client*> operators;
+        //booleans for modes inviteOnly (+i), topicRestricted (+t), etc.....
+
+    public:
+        Channel(const std::string& name);
+        ~Channel();
+
+        const std::string& getName() const;
+        const std::string& getTopic() const;
+        void setTopic(const std::string& newTopic);
+
+        void addMember(Client* client);
+        void removeMember(Client* client);
+        void addOperator(Client* client);
+        
+        bool isMember(Client* client) const;
+        bool isOperator(Client* client) const;
+        
+        void broadcastMessage(const std::string& message, Client* sender);
+};
+
+#endif
