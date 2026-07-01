d6d9290c437d34c75693825e58bd112ac7de647c
Inasse Eddaoudy <ieddaoud@e2r3p12.1337.ma>
2026-07-01 16:38:02 +0100

    pushh

--- Commands/Kick.cpp
+++ Commands/Kick.cpp
@@ -7,21 +7,21 @@ void Server::handleKick(int clientFd, std::vector<std::string>& cmds)
         sendMsg(clientFd, 461, " KICK :Not enough parameters");
     else 
     {
+        std::string channel = cmds[1];
         std::string nick = cmds[2];
         std::string reason = cmds[3];
-        std::string channel = cmds[1];
         if (CheckExist(clientFd, channel))
         {
             std::map<std::string, Channel>::iterator it = channels.find(channel);
             std::map<int,Client>::iterator it1 = clients.begin();
-            for(it1 = clients.begin(); it1 != clients.end(); ++it)
+            for(it1 = clients.begin(); it1 != clients.end(); ++it1)
             {
                 if (it1->second.getNickname() == nick)
                     break;
             }
             if (it1 == clients.end())
                 sendMsg(clientFd, 401, nick + " :No such nick/channel");
-            else if (!(it->second.isMember(&(it1->second))) )
+            else if (!(it->second.isMember(&(it1->second))))
                 sendMsg(clientFd, 441, nick + " " + channel + " :They aren't on that channel");
             else
             {
